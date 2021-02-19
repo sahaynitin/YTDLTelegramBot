a48f5d7d1e06ee5e46bde0234128c8d61289e65f
Adrian Galisteo <agaliste@Adrians-MacBook-Pro.local>
2021-02-19 23:30:39 +0100

    Added the possibility to download on mp3 + Updated help

--- bot.py
+++ bot.py
@@ -6,15 +6,16 @@ bot = telebot.TeleBot("***REMOVED***")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.reply_to(message, "Hi!! Welcome to the Downloader Bot made by @galisteo02!!")
+	bot.reply_to(message, "Hi!! Welcome to the YTDL Bot made by @galisteo02!!")
 	
 @bot.message_handler(commands=['help'])
 def help_message(message):
 	bot.reply_to(message,
-	'/help: Display all commands available\n' +
-	'/start: Display a greetings message and the creators @ \n' +
-	'/dl: Download a video [/dl url]'
-    '/id: Get the video ID'
+	'/start: Display a welcome message and credits\n'+
+	'/help: Display all commands available with descriptions\n' +
+	'/dl: Download a video [/dl URL]\n' +
+    '/dlmp3: Download a video on mp3 [/dlmp3 URL]\n' + 
+    '/id: Get the video ID [/id URL]'
 	)
 
 @bot.message_handler(commands=['dl'], content_types=['text'])
@@ -31,6 +32,30 @@ def dl(message):
     video = open(video_id[-1] + '.mp4', 'rb')
     bot.send_video(***REMOVED***, video)
     os.remove(video_id[-1] + '.mp4')
+    bot.send_message(***REMOVED***, 'Done!')
+
+@bot.message_handler(commands=['dlmp3'], content_types=['text'])
+def dlmp3(message):
+    bot.send_message(***REMOVED***, 'On the way!')
+    url = re.findall(r'(https?://\S+)', message.text)
+    audio_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text)
+    ydl_opts = {
+        'outtmpl': '%(id)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+        }
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
+    bot.send_message(***REMOVED***, 'Downloading... (This could take a while)')
+    with ydl:
+        ydl.download(url)
+    bot.send_message(***REMOVED***, 'Sending audio...')
+    audio = open(audio_id[-1] + '.mp3', 'rb')
+    bot.send_audio(***REMOVED***, audio)
+    os.remove(audio_id[-1] + '.mp3')
     bot.send_message(***REMOVED***, 'Done!')
 
 @bot.message_handler(commands=['id'], content_types=['text'])
