81309d1d57273cdadacae64b35b4a70030549293
somedevv <adriancrmini@gmail.com>
2021-02-21 03:29:25 +0100

    Fixed wrong variable name + Changed audio_id/video_id to c_id for simplicity

--- bot.py
+++ bot.py
@@ -36,38 +36,38 @@ def help_message(message):
 @bot.message_handler(commands=['dl'], content_types=['text']) #CHECK FOR /dl
 def dl(message):
     url = re.findall(r'(https?://\S+)', message.text) #PARSE URL
-    video_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text) #GET VIDEO ID
+    c_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text) #GET CONTENT ID
 
     if url and is_supported(url[-1]): #CHECK IF URL EXIST AND URL IS SUPPORTED
-        print_log('dl', 'OK', message.chat.id, video_id, url, message)
+        print_log('dl', 'OK', message.chat.id, c_id, url, message)
         progress_msg(message.chat.id, 1)
-        ydl_opts = {'outtmpl': video_id[-1] + '.%(ext)s'}
+        ydl_opts = {'outtmpl': c_id[-1] + '.%(ext)s'}
         ydl = youtube_dl.YoutubeDL(ydl_opts)
         progress_msg(message.chat.id, 2)
         try:
             with ydl:
                 ydl.download(url)
         except youtube_dl.utils.DownloadError:
-            print_log('dl', 'D_ERROR', message.chat.id, audio_id, url, message)
+            print_log('dl', 'D_ERROR', message.chat.id, c_id, url, message)
             return
         progress_msg(message.chat.id, 3)
-        video = open(video_id[-1] + '.mp4', 'rb')
+        video = open(c_id[-1] + '.mp4', 'rb')
         bot.send_video(message.chat.id, video)
-        os.remove(video_id[-1] + '.mp4')
+        os.remove(c_id[-1] + '.mp4')
         progress_msg(message.chat.id, 4)
     else:
-        print_log('dl', 'ERROR', message.chat.id, video_id, url, message)
+        print_log('dl', 'ERROR', message.chat.id, c_id, url, message)
 
 @bot.message_handler(commands=['dlmp3'], content_types=['text']) #CHECK FOR /dlmp3
 def dlmp3(message):
     url = re.findall(r'(https?://\S+)', message.text) #PARSE URL
-    audio_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text) #GET AUDIO ID
+    c_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text) #GET CONTENT ID
 
     if url and is_supported(url[-1]): #CHECK IF URL EXIST AND URL IS SUPPORTED
-        print_log('dlmp3', 'OK', message.chat.id, audio_id, url, message)
+        print_log('dlmp3', 'OK', message.chat.id, c_id, url, message)
         progress_msg(message.chat.id, 1)
         ydl_opts = {
-            'outtmpl': audio_id[-1] + '.%(ext)s',
+            'outtmpl': c_id[-1] + '.%(ext)s',
             'format': 'bestaudio/best',
             'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -80,27 +80,27 @@ def dlmp3(message):
             with ydl:
                 ydl.download(url)
         except youtube_dl.utils.DownloadError:
-            print_log('dlmp3', 'D_ERROR', message.chat.id, audio_id, url, message)
+            print_log('dlmp3', 'D_ERROR', message.chat.id, c_id, url, message)
             return
         progress_msg(message.chat.id, 3)
-        audio = open(audio_id[-1] + '.mp3', 'rb')
+        audio = open(c_id[-1] + '.mp3', 'rb')
         bot.send_audio(message.chat.id, audio)
-        os.remove(audio_id[-1] + '.mp3')
+        os.remove(c_id[-1] + '.mp3')
         progress_msg(message.chat.id, 4)
 
     else:
-        print_log('dlmp3', 'ERROR', message.chat.id, audio_id, url, message)
+        print_log('dlmp3', 'ERROR', message.chat.id, c_id, url, message)
 
 @bot.message_handler(commands=['id'], content_types=['text']) #CHECK FOR /id
 def send_video_id(message):
     url = re.findall(r'(https?://\S+)', message.text) #PARSE URL
-    video_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text) #GET VIDEO ID
+    c_id = re.findall(r"(?<![\"=\w])(?:[^\W_]+)(?![\"=\w]+)", message.text) #GET CONTENT ID
 
     if url and is_supported(url[-1]):
-        print_log('id', 'OK', message.chat.id, video_id, url, message)
+        print_log('id', 'OK', message.chat.id, c_id, url, message)
         bot.send_message(message.chat.id, '⚠️ DISCLAIMER ⚠️\n'+ '\n' + '🆔 not always corresponds to the real one!!')
-        bot.send_message(message.chat.id, '🆔 ==> ' + video_id[-1])
+        bot.send_message(message.chat.id, '🆔 ==> ' + c_id[-1])
     else:
-        print_log('id', 'ERROR', message.chat.id, video_id, url, message)
+        print_log('id', 'ERROR', message.chat.id, c_id, url, message)
 
 bot.polling()
